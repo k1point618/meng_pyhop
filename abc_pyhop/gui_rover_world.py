c78ded21a63f1339ffcdf061f57cd59b34a490c0
Keren Gu <kerengu@30-31-218.dynamic.csail.mit.edu>
2015-02-19 14:59:50 -0500

    gui immplementation and improvements

--- abc_pyhop/gui_rover_world.py
+++ abc_pyhop/gui_rover_world.py
@@ -1,57 +1,53 @@
 import Tkinter
 
 class rover_world_gui(Tkinter.Tk):
-    def __init__(self,parent,agent_init_world, real_world, solutions):
+    def __init__(self,parent, simulation):
         Tkinter.Tk.__init__(self,parent)
         self.parent = parent
-        self.agent_init_world = agent_init_world
-        self.real_world = real_world
-        self.solutions = solutions
-        self.cur_solution_idx = 0
-        self.cur_actions, self.cur_states = self.solutions[self.cur_solution_idx]
+
+        self.simulation = simulation
+
+        self.real_world = simulation.real_world
+
+        self.CUR_COL = real_world.prop['num_col']
+        self.CUR_ROW = real_world.prop['num_row']
+
+        self.solutions = None
+        self.rovers = []
+        self.agent_worlds = {}
+        self.agent_solutions = {}
+
         self.initialize(real_world)
 
+    def add_rover(self, rover_name, rover_world, solution): # Takes in 1 solution
+        self.rovers.append(rover_name)
+        self.agent_worlds[rover_name] = rover_world
+        self.agent_solutions[rover_name] = solution
+
+        self.actionFrame.initialize_agent_plans(rover_name, solution)
+
+
     def initialize(self, world):
-        self.grid()
+        self.boardFrame = BoardFrame(self, world)
+        self.boardFrame.pack(side=Tkinter.LEFT)
 
-        # Create widgets for the board
-        num_col = world.prop['num_col']
-        num_row = world.prop['num_row']
-        self.board_var = {}
-        for i in range(num_row):
-            for j in range(num_col):
-                self.board_var[(i, j)] = Tkinter.StringVar()
-                self.board_var[(i, j)].set("|\t|")
-                label = Tkinter.Label(self, textvariable=self.board_var[(i, j)], 
-                    text="empty", fg='black', bg='gray',anchor=Tkinter.CENTER, height=3)
-                label.grid(column=j, row=i, sticky='EWSN')
-
-        CUR_ROW=num_row
-        # Add objects onto the board
-        for (obj, loc) in world.at.items():
-            cur_x, cur_y = world.loc[loc]
-            self.board_var[(cur_x, cur_y)].set(obj)
+        self.actionFrame = AgentActionsFrame(self)
+        self.actionFrame.pack(side=Tkinter.LEFT)
 
         # Add next-button to board
-        button_nextstep = Tkinter.Button(self,text=u"Next Step", command=self.NextStep)
-        button_nextstep.grid(column=0,row=CUR_ROW,columnspan=3)
-        CUR_ROW += 1
+        self.nextButton = Tkinter.Button(self,text=u"Next", command=self.NextStep)
+        self.nextButton.pack(side=Tkinter.BOTTOM)
 
-        # Labels for showing progress
-        self.next_action = Tkinter.StringVar()
-        self.next_action.set(self.cur_actions[0])
-        label_next_action = Tkinter.Label(self, textvariable=self.next_action)
-        label_next_action.grid(column=0, row=CUR_ROW)
-
-
+        
         # For Re-sizing
-        for i in range(num_col):
+        for i in range(self.CUR_COL):
             self.grid_columnconfigure(i,weight=1)
 
         self.resizable(True,False)
 
     def NextStep(self):
-        pass
+        new_world = self.simulation.step()
+        self.boardFrame.update_board(new_world)
 
     def OnButtonClick(self):
         print "You clicked the button !"
@@ -62,6 +58,79 @@ class rover_world_gui(Tkinter.Tk):
 
     def set_solutions(self, solutions):
         this.solutions = solutions
+
+
+# For the actions Frame (right of the board)
+class AgentActionsFrame(Tkinter.Frame):
+    def __init__(self, parent):
+        Tkinter.Frame.__init__(self, parent, background="blue")
+        # self.initialize_agent_plans('none', (['test'], ['test'])) # Emmpty one
+
+    def initialize_agent_plans(self, rover_name, solution):
+        
+        scrollbar = Tkinter.Scrollbar(self)
+        scrollbar.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+
+        listBox = Tkinter.Listbox(self)
+        listBox.pack(side=Tkinter.LEFT, fill=Tkinter.Y)
+        
+        # Header Label for agent's name
+        # Tkinter.Label(self, text=rover_name).grid(column=self.CUR_COL, row=0)
+        listBox.insert(Tkinter.END, rover_name)
+
+        # Show agent's plans
+        (plan, states) = solution
+        for action in plan:
+            # Tkinter.Label(self, text=plan[cur_row-1]).grid(column=self.CUR_COL, row=cur_row)
+            listBox.insert(Tkinter.END, action)
+
+        scrollbar.config(command=listBox.yview)
+
+        # self.next_action = Tkinter.StringVar()
+        # self.next_action.set(self.cur_actions[0])
+        # label_next_action = Tkinter.Label(self, textvariable=self.next_action)
+        # label_next_action.grid(column=CUR_COL, row=0)
+
+
+# For the main board frame (Top left)
+class BoardFrame(Tkinter.Frame):
+    def __init__(self, parent, world):
+        Tkinter.Frame.__init__(self, parent, background="black")
+        self.grid()
+        self.initialize_board(world)
+
+
+    def initialize_board(self, world):
+        # Create widgets for the board
+        num_col = world.prop['num_col']
+        num_row = world.prop['num_row']
+        self.board_var = {}
+        for i in range(num_row):
+            for j in range(num_col):
+                self.board_var[(i, j)] = Tkinter.StringVar()
+                self.board_var[(i, j)].set("|\t|")
+                label = Tkinter.Label(self, textvariable=self.board_var[(i, j)], 
+                    text="empty", fg='black', bg='gray',anchor=Tkinter.CENTER, height=4)
+                label.grid(column=j, row=i, sticky='EWSN')
+
+        # Add objects onto the board
+        for (obj, loc) in world.at.items():
+            cur_x, cur_y = world.loc[loc]
+            self.board_var[(cur_x, cur_y)].set(obj)
+
+    def update_board(self, world):
+
+        # Create widgets for the board
+        num_col = world.prop['num_col']
+        num_row = world.prop['num_row']
+        for i in range(num_row):
+            for j in range(num_col):
+                self.board_var[(i, j)].set("|\t|")
+                
+        # Add objects onto the board
+        for (obj, loc) in world.at.items():
+            cur_x, cur_y = world.loc[loc]
+            self.board_var[(cur_x, cur_y)].set(obj)
 
 if __name__ == "__main__":
     app = rover_world_gui(None)
--- abc_pyhop/simulate_rovers_world.py
+++ abc_pyhop/simulate_rovers_world.py
@@ -21,124 +21,185 @@ from random_rovers_world import *
 from gui_rover_world import *
 
 
-"""
-+ Single Agent solving a problem with no uncertainty
-+ Uncertainty can be toggled ON
-"""
-def single_agent_run(uncertainty=False, verbose=0, show_run=True, a_star=True, gui=True):
-    world = get_random_world()
-    world.settings['a-star'] = a_star
+class Simulation():
 
-    print('start state')
-    print_state(world)
-    print_board(world)
-
-    # Samples 1 solution for this problem
-    solutions = pyhop(world, 'agent1', 0, all_solutions=False, amortize=False)
-    
-    # Real world has possible uncertainties.
-    real_world = copy.deepcopy(world)
-    agent_init_world = copy.deepcopy(world)
-
-    # Initialize Gui
-    if gui:
-        app = rover_world_gui(None, agent_init_world, real_world, solutions)
-        app.title('Rovers World GUI')
-        app.mainloop()
-
-    elif show_run:
-        for (i, solution) in enumerate(solutions):
-            print('*** Showing plan #{} of {}'.format(i+1, len(solutions)))
-            show_single_agent(agent_init_world, real_world, solution, 'agent1')
-
-
-""" temporary hack for testing """
-def remove_traps(world):
-    for key in world.loc_available:
-        world.loc_available[key] = True
-    return world
-
-
-def show_single_agent_recurse(cur_world, real_world, solution, agent, uncertainty=False):
-    (plan, states) = solution
-
-    # Take 1 step at a time
-    for (i, cur_action) in enumerate(plan):
-
-        # 0: Info
-        print('length of plan: {}; length of states: {}'.format(len(plan), len(states)))
-        print('\ttimestep: {}; \n\tactions: {};'.format(i, plan[i:]))
-
-        # 1: Generate possible Uncertainty to the real-world
-        generate_uncertainty(real_world, a_prob=1, verbose=True)
-
-        # 2: This agent get observation about surrounding world and decides to replan
-        replan = get_observation(agent, states[i], cur_action, real_world)
-
-        # 3: Agent MIGHT need to re-plan.
-        if replan:
-            raw_input("Need to re-plan...")
-            # Do replanning Stuff
-            # TODO: Include the problem/goal in State/World definition
-            print('replanning')
-            # TODO: Update domain definition (preconditions) so that if task is done, 
-            # no need to re-do
-            real_world = remove_traps(copy.deepcopy(real_world))
-            print('remove_traps...')
-            print_board(real_world)
-
-            solutions = pyhop(real_world, 'agent1', verbose=3, all_solutions=False, amortize=False)
-            solution = solutions[0]
-            print('new solution', solution)
-
-            if solution != False: 
-                show_single_agent(real_world, real_world, solution, agent)
-                return
-            else:
-                print('no solution found for agent:{}, goal:{}'.format(agent, real_world.goals[agent]))
-                return
-        # 4: (if not replan) Agent takes action
-        next_state = act(cur_world, cur_action) # This is the same as states[i]
-        real_world = act(real_world, cur_action)
-
-        # Infity: Info
-        print('next state')
-        print_board(states[i])
-        print('real world')
-        print_board(real_world)
-
-        raw_input("Press Enter to continue...")
-
-
-def show_single_agent(cur_world, real_world, solution, agent, uncertainty=False):
-    (actions, states) = solution
-
-    # Take 1 step at a time
-    step_counter = 0
-    while len(actions) != 0:
+    def __init__(self, uncertainty=False, verbose=0, show_run=True, a_star=True, gui=True):
+        # Simulation parameters: 
         
-        # 0: Info
-        print('length of remaining plan: {}; \nlength of remaining states: {}'
-            .format(len(actions), len(states)))
-        print('\ttimestep: {}; \n\tactions: {};'.format(step_counter, actions))
+        self.step_counter = 0
+        world = get_random_world()
+        world.settings['a-star'] = a_star
+        
+        print('start state')
+        print_state(world)
+        print_board(world)
 
-        cur_action = actions.pop(0)
-        next_state = states.pop(0)
+        # Samples 1 solution for this problem
+        a1_solutions = pyhop(world, 'agent1', 0, all_solutions=False, amortize=False)
+        
+        # Real world has possible uncertainties.
+        real_world = copy.deepcopy(world)
+        agent_init_world = copy.deepcopy(world)
+        
+        self.real_world = world
+        self.agent_init_world = agent_init_world
+        self.agent_solution = a1_solutions[0] # TODO: Just 1 solution for now
+        self.actions, self.states = a1_solutions[0]
+
+        # Initialize Gui
+        if gui:
+            app = rover_world_gui(None, self)
+            app.title('Rovers World GUI')
+            app.add_rover('agent1', agent_init_world, a1_solutions[0]) #Just 1 solution for now
+            app.add_rover('agent2', copy.deepcopy(agent_init_world), a1_solutions[0])
+            app.mainloop()
+            self.gui = app
+
+        elif show_run:
+            for (i, solution) in enumerate(a1_solutions):
+                print('*** Showing plan #{} of {}'.format(i+1, len(a1_solutions)))
+                Simulation.show_single_agent(agent_init_world, real_world, solution, 'agent1')
+
+   
+    """ temporary hack for testing """
+    def remove_traps(world):
+        for key in world.loc_available:
+            world.loc_available[key] = True
+        return world
+
+    @staticmethod
+    def show_single_agent_recurse(cur_world, real_world, solution, agent, uncertainty=False):
+        (plan, states) = solution
+
+        # Take 1 step at a time
+        for (i, cur_action) in enumerate(plan):
+
+            # 0: Info
+            print('length of plan: {}; length of states: {}'.format(len(plan), len(states)))
+            print('\ttimestep: {}; \n\tactions: {};'.format(i, plan[i:]))
+
+            # 1: Generate possible Uncertainty to the real-world
+            generate_uncertainty(real_world, a_prob=1, verbose=True)
+
+            # 2: This agent get observation about surrounding world and decides to replan
+            replan = get_observation(agent, states[i], cur_action, real_world)
+
+            # 3: Agent MIGHT need to re-plan.
+            if replan:
+                raw_input("Need to re-plan...")
+                # Do replanning Stuff
+                # TODO: Include the problem/goal in State/World definition
+                print('replanning')
+                # TODO: Update domain definition (preconditions) so that if task is done, 
+                # no need to re-do
+                real_world = remove_traps(copy.deepcopy(real_world))
+                print('remove_traps...')
+                print_board(real_world)
+
+                solutions = pyhop(real_world, 'agent1', verbose=3, all_solutions=False, amortize=False)
+                solution = solutions[0]
+                print('new solution', solution)
+
+                if solution != False: 
+                    show_single_agent(real_world, real_world, solution, agent)
+                    return
+                else:
+                    print('no solution found for agent:{}, goal:{}'.format(agent, real_world.goals[agent]))
+                    return
+            # 4: (if not replan) Agent takes action
+            next_state = act(cur_world, cur_action) # This is the same as states[i]
+            real_world = act(real_world, cur_action)
+
+            # Infity: Info
+            print('next state')
+            print_board(states[i])
+            print('real world')
+            print_board(real_world)
+
+            raw_input("Press Enter to continue...")
+
+    @staticmethod
+    def show_single_agent(cur_world, real_world, solution, agent, uncertainty=False):
+        (actions, states) = solution
+
+        # Take 1 step at a time
+        step_counter = 0
+        while len(actions) != 0:
+            
+            print("*** Old stuff ***")
+            # 0: Info
+            print('length of remaining plan: {}; \nlength of remaining states: {}'
+                .format(len(actions), len(states)))
+            print('\ttimestep: {}; \n\tactions: {};'.format(step_counter, actions))
+
+            cur_action = actions.pop(0)
+            next_state = states.pop(0)
+
+            # 1: Generate possible Uncertainty to the real-world
+            generate_uncertainty(real_world, a_prob=1, verbose=True)
+
+            # 2: This agent get observation about surrounding world and decides to replan
+            replan = get_observation(agent, next_state, cur_action, real_world)
+
+            # 3: Agent MIGHT need to re-plan.
+            if replan:
+                raw_input("Need to re-plan...")
+                print('replanning')
+
+                print_board(real_world)
+
+                solutions = pyhop(real_world, 'agent1', verbose=0, all_solutions=False, amortize=False)
+                solution = solutions[0]
+                
+                # print('new solution', solution)
+
+                if solution != False: 
+                    (actions, states) = solution
+
+                else:
+                    print('no solution found for agent:{}, goal:{}'.format(agent, real_world.goals[agent]))
+                    return
+
+            else:
+                # 4: (if not replan) Agent takes action
+                # next_state = act(cur_world, cur_action) # This is the same as states[i]
+                real_world = act(real_world, cur_action)
+
+                # end: Info
+                print('next state')
+                print_board(next_state)
+                print('real world')
+                print_board(real_world)
+
+                raw_input("Press Enter to continue...")
+
+            step_counter += 0
+
+    def step(self, agent='agent1'):
+        # 0: Info
+        print("*** Next Step ***")
+        print('length of remaining plan: {}; \nlength of remaining states: {}'
+            .format(len(self.actions), len(self.states)))
+        print('\ttimestep: {}; \n\tactions: {};'.format(self.step_counter, self.actions))
+
+
+        cur_action = self.actions.pop(0)
+        next_state = self.states.pop(0)
 
         # 1: Generate possible Uncertainty to the real-world
-        generate_uncertainty(real_world, a_prob=1, verbose=True)
+        # generate_uncertainty(real_world, a_prob=1, verbose=True)
 
         # 2: This agent get observation about surrounding world and decides to replan
-        replan = get_observation(agent, next_state, cur_action, real_world)
-
+        # replan = get_observation(agent, next_state, cur_action, real_world)
+        replan=False
         # 3: Agent MIGHT need to re-plan.
         if replan:
             raw_input("Need to re-plan...")
             print('replanning')
 
-            print_board(real_world)
+            print_board(self.real_world)
 
-            solutions = pyhop(real_world, 'agent1', verbose=0, all_solutions=False, amortize=False)
+            solutions = pyhop(self.real_world, 'agent1', verbose=0, all_solutions=False, amortize=False)
             solution = solutions[0]
             
             # print('new solution', solution)
@@ -147,24 +208,25 @@ def show_single_agent(cur_world, real_world, solution, agent, uncertainty=False)
                 (actions, states) = solution
 
             else:
-                print('no solution found for agent:{}, goal:{}'.format(agent, real_world.goals[agent]))
+                print('no solution found for agent:{}, goal:{}'.format(agent, self.real_world.goals[agent]))
                 return
 
         else:
             # 4: (if not replan) Agent takes action
             # next_state = act(cur_world, cur_action) # This is the same as states[i]
-            real_world = act(real_world, cur_action)
+            print('cur_action: ', cur_action)
+            self.real_world = act(self.real_world, cur_action)
 
             # end: Info
             print('next state')
             print_board(next_state)
             print('real world')
-            print_board(real_world)
+            print_board(self.real_world)
 
-            raw_input("Press Enter to continue...")
+            # raw_input("Press Enter to continue...")
 
-        step_counter += 0
+        self.step_counter += 1
+        return self.real_world
 
-
-single_agent_run()
+simulation = Simulation()
 
